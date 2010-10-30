2fbdc5009c120e68b63b34f8f8a9c402bac728e7
10sr <sr10@users.sourceforge.jp>
2010-10-31 00:37:47 +0900

    added smallgit-mode.el

---
+++ smallgit-mode.el
@@ -0,0 +1,35 @@
+(defun smallgit-mode ()
+  ""
+  (interactive))
+
+(defun smallgit--call-git (&rest ARGS)
+  "not needed?"
+  (apply 'call-process "git" nil nil nil ARGS))
+
+(defun smallgit-init ()
+  ""
+  (interactive)
+  (call-process "git" nil nil nil "init"))
+
+(defun smallgit-add (&optional file)
+  ""
+  (interactive)
+  (call-process "git" nil nil nil "add" (or file (buffer-file-name))))
+
+(defun smallgit-add-all ()
+  ""
+  (interactive)
+  (call-process "git" nil nil nil "add" "-a"))
+
+;; (defun smallgit-update-index-add (&optional file)
+;;   ""
+;;   (start-process "git" nil nil nil "update-index" "-add" file))
+
+(defun smallgit-commit-all ()
+  ""
+  (interactive)
+  (smallgit-add-all)
+  (log-edit ((call-process "git" nil nil nil "commit"))
+
+
+
