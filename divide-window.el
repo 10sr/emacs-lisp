7b33982cbae2c7e0811318dcd099719812219243
10sr <8slashes+git@gmail.com>
2012-10-12 11:34:38 +0900

    add divide-window

---
+++ divide-window.el
@@ -0,0 +1,46 @@
+(defvar divide-window-default-num
+  3
+  "Default number of windows `divide-window-vertically' and
+`divide-window-horizontally' make.")
+
+(defun divide-window-vertically (arg)
+  "Divide window equally vertically."
+  (interactive "p")
+  (let ((n (if (eq arg 1)
+               (or divide-window-default-num
+                   3)
+             arg))
+        (height (window-height)))
+    (divide-window--internal nil
+                             n
+                             (/ height n)
+                             nil)))
+
+(defun divide-window-horizontally (arg)
+  "Divide window equally horizontally."
+  (interactive "p")
+  (let ((n (if (eq arg 1)
+               (or divide-window-default-num
+                   3)
+             arg))
+        (width (window-width)))
+    (divide-window--internal nil
+                             n
+                             (/ width n)
+                             t)))
+
+(defun divide-window--internal (window num size side)
+  "Used for internal"
+  (message "%d" num)
+  (when (not (eq num
+                 1))
+    (let ((nw (split-window window
+                            size
+                            side)
+              ))
+      (divide-window--internal nw
+                               (- num 1)
+                               size
+                               side))))
+
+(provide 'divide-window)
