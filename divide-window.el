eb4bf77e4ea0e9dd65b95e21c68a170c6aafda34
10sr <8slashes+git@gmail.com>
2012-10-13 13:38:00 +0900

    add arg window

--- divide-window.el
+++ divide-window.el
@@ -3,28 +3,28 @@
   "Default number of windows `divide-window-vertically' and
 `divide-window-horizontally' make.")
 
-(defun divide-window-vertically (arg)
+(defun divide-window-vertically (arg &optional window)
   "Divide window equally vertically."
   (interactive "p")
   (let ((n (if (eq arg 1)
                (or divide-window-default-num
                    3)
              arg))
-        (height (window-height)))
-    (divide-window--internal nil
+        (height (window-height window)))
+    (divide-window--internal window
                              n
                              (/ height n)
                              nil)))
 
-(defun divide-window-horizontally (arg)
+(defun divide-window-horizontally (arg &optional window)
   "Divide window equally horizontally."
   (interactive "p")
   (let ((n (if (eq arg 1)
                (or divide-window-default-num
                    3)
              arg))
-        (width (window-width)))
-    (divide-window--internal nil
+        (width (window-width window)))
+    (divide-window--internal window
                              n
                              (/ width n)
                              t)))
