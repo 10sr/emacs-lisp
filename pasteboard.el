4930261d03bdd3b1d0d5a0f1cd8773c36aebef16
10sr <8slashes+git@gmail.com>
2012-10-14 16:17:48 +0900

    add pasteboard-disable-rtun

--- pasteboard.el
+++ pasteboard.el
@@ -5,7 +5,9 @@
 (defvar pasteboard-copy-program (executable-find "pbcopy")
   "Program to put text to osx pasteboard.")
 (defvar pasteboard-rtun-program (executable-find "reattach-to-user-namespace")
-  "Program reattach-to-user-namespace")
+  "Program reattach-to-user-namespace. It is needed when you use pasteboard
+within tmux. For details see
+https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard")
 
 (defvar pasteboard-paste-command (list pasteboard-paste-program)
   "Command run to get text.")
@@ -24,15 +26,27 @@
   (setq interprogram-paste-function nil)
   (setq interprogram-cut-function nil))
 
-(defun pasteboard-use-rtun ()
+(defun pasteboard-enable-rtun ()
   "Set to use reattach-to-user-namespace."
   (interactive)
+  (if pasteboard-rtun-program
+      (progn
+        (setq pasteboard-paste-command
+              (list pasteboard-rtun-program
+                    pasteboard-paste-program))
+        (setq pasteboard-copy-command
+              (list pasteboard-rtun-program
+                    pasteboard-copy-program)))
+    (message
+     "Cannot find reattach-to-user-namespace. First you must install it!")))
+
+(defun pasteboard-disable-rtun ()
+  "Set not to use reattach-to-user-namespace."
+  (interactive)
   (setq pasteboard-paste-command
-        (list pasteboard-rtun-program
-              pasteboard-paste-program))
+        (list pasteboard-paste-program))
   (setq pasteboard-copy-command
-        (list pasteboard-rtun-program
-              pasteboard-copy-program)))
+        (list pasteboard-copy-program)))
 
 (defun pasteboard-paste ()
   (apply 'call-process
