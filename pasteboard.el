5a06a8af8fe7fc0466c6fad3edf3462f255a5a77
10sr <8slashes+git@gmail.com>
2012-10-14 16:10:53 +0900

    fix bug of pasteboard in tmux

--- pasteboard.el
+++ pasteboard.el
@@ -4,17 +4,13 @@
   "Program to get text from osx pasteboard.")
 (defvar pasteboard-copy-program (executable-find "pbcopy")
   "Program to put text to osx pasteboard.")
+(defvar pasteboard-rtun-program (executable-find "reattach-to-user-namespace")
+  "Program reattach-to-user-namespace")
 
-(defun pasteboard-paste ()
-  (shell-command-to-string pasteboard-paste-program))
-
-(defun pasteboard-cut (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process pasteboard-copy-program
-                               "*Messages*"
-                               pasteboard-copy-program)))
-      (process-send-string proc text)
-      (process-send-eof proc))))
+(defvar pasteboard-paste-command (list pasteboard-paste-program)
+  "Command run to get text.")
+(defvar pasteboard-copy-command (list pasteboard-copy-program)
+  "Command run to put text.")
 
 (defun turn-on-pasteboard ()
   "Enable pasteboard for yank and paste."
@@ -27,5 +23,32 @@
   (interactive)
   (setq interprogram-paste-function nil)
   (setq interprogram-cut-function nil))
+
+(defun pasteboard-use-rtun ()
+  "Set to use reattach-to-user-namespace."
+  (interactive)
+  (setq pasteboard-paste-command
+        (list pasteboard-rtun-program
+              pasteboard-paste-program))
+  (setq pasteboard-copy-command
+        (list pasteboard-rtun-program
+              pasteboard-copy-program)))
+
+(defun pasteboard-paste ()
+  (apply 'call-process
+         (car pasteboard-paste-command)
+         nil
+         t
+         t
+         (cdr pasteboard-paste-command)))
+
+(defun pasteboard-cut (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (apply 'start-process
+                       pasteboard-copy-program
+                       "*Messages*"
+                       pasteboard-copy-command)))
+      (process-send-string proc text)
+      (process-send-eof proc))))
 
 (provide 'pasteboard)
