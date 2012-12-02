297d8d140b6cf80fcca5a4a388330d1a9f7f1fed
10sr <8slashes+git@gmail.com>
2012-12-03 02:12:38 +0900

    add function smart-revert-revert

--- smart-revert.el
+++ smart-revert.el
@@ -1,16 +1,23 @@
 (defvar smart-revert--last-buffer nil)
 
 (defun smart-revert ()
-  "Revert current buffer when and only when changes are found."
-  (interactive)
+  "Call `smart-revert-revert' if current buffer is changed since last call."
   (unless (eq smart-revert--last-buffer (current-buffer))
     (setq smart-revert--last-buffer (current-buffer))
-    (when (or (and (eq major-mode 'dired-mode)
-                   (dired-directory-changed-p default-directory))
-              (not (verify-visited-file-modtime (current-buffer))))
-      (revert-buffer t t)
-      (message "%s reverted." (buffer-name))
-      )))
+    (smart-revert-revert)))
+
+(defun smart-revert-revert ()
+  "Revert current buffer when and only when changes are found."
+  (interactive)
+  (when (or (and (eq major-mode 'dired-mode)
+                 (dired-directory-changed-p default-directory))
+            (not (verify-visited-file-modtime (current-buffer))))
+    (if (file-readable-p buffer-file-name)
+        (progn
+          (revert-buffer t t)
+          (message "%s reverted." (buffer-name)))
+      (message "%s not found."
+               buffer-file-nmae))))
 
 (defun smart-revert-on ()
   "Enable `smart-revert'"
