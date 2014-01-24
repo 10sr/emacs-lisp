952f9d5bcf5c8bb2123b08ae3b13ece0dfdb6098
10sr <8slashes+git@gmail.com>
2014-01-25 02:47:28 +0900

    update pack.el

--- pack.el
+++ pack.el
@@ -1,4 +1,47 @@
-(require 'simple)
+;;; pack.el --- Pack and unpack archive files
+
+;; Author: 10sr <>
+;; URL: https://github.com/10sr/emacs-lisp/blob/master/pack.el
+;; Version: 0.1
+;; Package-Requires: ()
+;; Keywords:
+
+;; This file is not part of GNU Emacs.
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+
+;; In jurisdictions that recognize copyright laws, the author or authors
+;; of this software dedicate any and all copyright interest in the
+;; software to the public domain. We make this dedication for the benefit
+;; of the public at large and to the detriment of our heirs and
+;; successors. We intend this dedication to be an overt act of
+;; relinquishment in perpetuity of all present and future rights to this
+;; software under copyright law.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
+
+;; For more information, please refer to <http://unlicense.org/>
+
+;;; Commentary:
+
+;; pack.el provides 3 functions: `pack-pack', `pack-unpack' and
+;; `dired-do-pack-or-unpack'.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'simple))
 
 (declare-function dired-dwim-target-directory "dired-aux")
 
@@ -7,12 +50,14 @@
       (executable-find "7za")
       (executable-find "7zr"))
   "Path to 7z program.")
+
 (defvar pack-buffer-name "*Pack*"
   "Buffer name for `pack'")
 
 (defvar pack-default-extension
   "7z"
-  "Default suffix for packing. Filename with this suffix must matches one of `pack-program-alist'.")
+  "Default suffix for packing.
+Filename with this suffix must matches one of `pack-program-alist'.")
 
 (defvar pack-program-alist
   `(
@@ -24,35 +69,38 @@
     )
   "Alist of filename patterns, command for pack and unpack.
 Each element looks like (REGEXP PACKING-COMMAND UNPACKING-COMMAND).
-PACKING-COMMAND and UNPACKING-COMMAND can be nil if the command is not available.
-Alist is searched from the beginning so pattern for \".tar.gz\" should be ahead of pattern for \".gz\"")
+PACKING-COMMAND and UNPACKING-COMMAND can be nil if the command is not
+available.  Alist is searched from the beginning so pattern for \".tar.gz\"
+should be ahead of pattern for \".gz\"")
 
 (defun dired-do-pack-or-unpack ()
   "Pack or unpack files in dired.
-If targetting one file and that is archive file defined in `pack-program-alist', unpack that.
+If targetting one file and that is archive file defined in `pack-program-alist',
+unpack that.
 Otherwise, pack marked files, prompting user to decide filename for archive."
   (interactive)
   (let* ((infiles (dired-get-marked-files t))
-         (onefile (and (eq 1 ; filename if only one file targeted, otherwise nil.
+         (onefile (and (eq 1 ; filename if only one file targeted, otherwise nil
                            (length infiles))
                        (car infiles))))
     (if (and onefile
              (pack-file-name-association onefile))
-        (when (y-or-n-p (format "unpack %s? " onefile))
-          (unpack onefile))
+        (when (y-or-n-p (format "Unpack %s? " onefile))
+          (pack-unpack onefile))
       (let* ((dir-default (if (require 'dired-aux nil t)
                               (dired-dwim-target-directory)
                             default-directory))
-             (archive-default (pack-file-extension (file-name-nondirectory (car infiles))))
+             (archive-default (pack-file-extension (file-name-nondirectory
+                                                    (car infiles))))
              (archive ;; (if (interactive-p)
-              (read-file-name "Output file to pack : "
+              (read-file-name "Archive file name: "
                               dir-default
                               nil
                               nil
                               archive-default)
               ;; (concat dir-default archive-default)
               ))
-        (apply 'pack
+        (apply 'pack-pack
                archive
                infiles))))
   (revert-buffer)
@@ -75,7 +123,7 @@ Otherwise, return FILENAME with `pack-default-extension'"
                    'string-match-p
                    nil)))
 
-(defun unpack (archive)
+(defun pack-unpack (archive)
   "Unpack ARCHIVE. Command for unpacking is defined in `pack-program-alist'"
   (interactive "fArchive to extract: ")
   (let* ((earchive (expand-file-name archive))
@@ -87,9 +135,10 @@ Otherwise, return FILENAME with `pack-default-extension'"
                                " "
                                (shell-quote-argument earchive))
                        (get-buffer-create pack-buffer-name))
-      (message "this is not archive file defined in `pack-program-alist'!"))))
+      (message "Cannot find unpacking command for %s"
+               archive))))
 
-(defun pack (archive &rest files)
+(defun pack-pack (archive &rest files)
   "Pack FILES into ARCHIVE.
 If ARCHIVE have extension defined in `pack-program-alist', use that command.
 Otherwise, use `pack-default-extension' for pack."
@@ -98,13 +147,16 @@ Otherwise, use `pack-default-extension' for pack."
          )
     (if cmd
         (async-shell-command (concat cmd
-                               " "
-                               (shell-quote-argument archive-ext)
-                               " "
-                               (mapconcat 'shell-quote-argument
-                                          files
-                                          " "))
-                       (get-buffer-create pack-buffer-name))
-      (message "invalid extension for packing!"))))
+                                     " "
+                                     (shell-quote-argument archive-ext)
+                                     " "
+                                     (mapconcat 'shell-quote-argument
+                                                files
+                                                " "))
+                             (get-buffer-create pack-buffer-name))
+      (message "Invalid extension for packing: %s"
+               archive-ext))))
 
 (provide 'pack)
+
+;;; pack.el ends here
