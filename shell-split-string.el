e76cb21f2f37e6060f63220e42caca392d900a23
10sr <8slashes+git@gmail.com>
2014-12-24 08:01:43 +0900

    Implement and add tests

--- shell-split-string.el
+++ shell-split-string.el
@@ -39,10 +39,110 @@
 
 ;;; Code:
 
-(defun shell-split-string (str)
-  "Split string STR using shell-like syntax."
-  (split-string str))
+(defvar shell-split-string-separators
+  '(?  ?\f ?\t ?\n ?\r ?\v)
+  "List of chars of separaters.")
 
+(defun shell-split-string (str)
+  "Split string STR using shell-like syntax and return resulting list."
+  (if (or (string-match-p "\"" str)
+          (string-match-p "'" str))
+      ;; If " nor ' is not included, simply split-string it
+      (shell--split-string-1 (mapcar 'identity str) nil nil nil)
+    (split-string str
+                  (concat "["
+                          (apply 'string shell-split-string-separators)
+                          "]+")
+                  t)))
+
+(defun shell--split-string-1 (rest current quote done)
+  "Split string using shell-like syntax and return resulting list.
+
+REST should be a list of chars that is not read yet.
+
+CURRENT is a list of chars of word currently processing.
+
+QUOTE is a char that quotes CURRENT word. Should be ?\", ?' or nil.
+The value nil means CURRENT word is not quoted.
+
+DONE is a list of strings that was already processed."
+  (if (not rest)
+      ;; If no chars to read left, return resulting list
+      `(,@done ,(apply 'string current))
+    (let ((first (car rest))
+          (rest (cdr rest)))
+      (cond
+       ((memq first shell-split-string-separators)
+        (if quote
+            ;; if cnrrently in inside of quotation, just append to current word
+            (shell--split-string-1 rest
+                                   `(,@current ,first)
+                                   quote
+                                   done)
+          ;; if outside of quotation, current word is terminated
+          (shell--split-string-1 rest
+                                 nil
+                                 nil
+                                 `(,@done ,(apply 'string current)))))
+
+       ((eq first ?\\)
+        ;; backslash
+        ;; if inside of quote and next char is the quote letter, append it
+        ;; if outside of quote, read next char literally
+        ;; if inside of quote, just append backslash to current word
+        (cond
+         (quote
+          (if (eq quote (car rest))
+              (let ((first (car rest))
+                    (rest (cdr rest)))
+                (shell--split-string-1 rest
+                                       `(,@current ,first)
+                                       quote
+                                       done))
+            (shell--split-string-1 rest
+                                   `(,@current ,first)
+                                   quote
+                                   done)))
+         ('otherwise
+          (let ((first (car rest))
+                (rest (cdr rest)))
+            (shell--split-string-1 rest
+                                   `(,@current ,first)
+                                   quote
+                                   done)))))
+
+       ((memq first '(?\" ?'))
+        (cond
+         ((eq first quote)
+          ;; end of quotation
+          (shell--split-string-1 rest
+                                 current
+                                 nil
+                                 done))
+         (quote
+          ;; quote does not match, but still quoted with another letter
+          ;; for example quoted with \", but read '
+          (shell--split-string-1 rest
+                                 `(,@current ,first)
+                                 quote
+                                 done))
+
+         ('otherwise
+          ;; if not quoted yet, start quoted word
+          (shell--split-string-1 rest
+                                 current
+                                 first
+                                 done))
+         ))
+
+       ('otherwise
+        (shell--split-string-1 rest
+                               `(,@current ,first)
+                               quote
+                               done))
+       ))))
+
+(shell--split-string-1 (mapcar 'identity "\"a\"") nil nil nil)
 
 (provide 'shell-split-string)
 
