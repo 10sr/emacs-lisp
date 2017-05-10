0cda4c9153460997f3ab7b7c8a6c2bf2ec3a05f9
10sr <8slashes+git@gmail.com>
2017-05-10 14:52:05 +0900

    Update doc

--- el/pack.el
+++ el/pack.el
@@ -35,8 +35,20 @@
 
 ;;; Commentary:
 
-;; pack.el provides 3 functions: `pack-pack', `pack-unpack' and
-;; `pack-dired-dwim'.
+;; This library provides some commands and functions to pack and unpack
+;; archives.
+
+;; Commands to pack/unpack archive files are defined in `pack-program-alist'.
+
+;; Use from Dired
+
+;; To pack/unpack files from dired buffers, add following to your init.el:
+
+;; (with-eval-after-load 'dired
+;;   (define-key dired-mode-map "P" 'pack-dired-dwim))
+
+;; Now you can create an archive file from marked files, or unpack the file when
+;; only one file is selected and that seems to be an archive.
 
 ;;; Code:
 
